6e82a39f44343f82283c70dcfb75dc05235c84fb
alan <alan@fanruan.com>
2022-08-29 10:13:26 +0800

    neovim 不显示tab栏

--- neovim/init.lua
+++ neovim/init.lua
@@ -5,7 +5,7 @@ require('colorscheme')
 require('keybindings')
 -- 插件配置
 require("plugin-config.nvim-tree")
--require("plugin-config.bufferline")
+--require("plugin-config.bufferline")
 require("plugin-config.lualine")
 require("plugin-config.dashboard")
 require("plugin-config.project")
